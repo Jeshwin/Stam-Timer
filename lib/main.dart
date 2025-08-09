9eccee3c5c2e3d15b16da9b72c09ca2307185752
Jeshwin Prince <jeshwinjprince@gmail.com>
2025-08-09 12:35:05 -0700

    Publish to Play Store only

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class StamFocusApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Stam Focus',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
